478062fc3f710c68413beed3defd69272e8d76b8
yannickaboh <yannickabohthierry@gmail.com>
2019-11-28 03:15:01 +0100

    setti, urls, templates

--- association/urls.py
+++ association/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^gestion/', include('gestion.urls')),
+    url(r'^$', include('gestion.urls')),
 
 ]
 
